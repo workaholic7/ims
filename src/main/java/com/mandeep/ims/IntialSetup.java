f7f678b7a230e92a02968b90ebf826389b138352
Anshul Agarwal <anshulagarwal0391@gmail.com>
2020-08-28 18:55:16 +0530

    Initial Setup

--- src/main/java/com/mandeep/ims/IntialSetup.java
+++ src/main/java/com/mandeep/ims/IntialSetup.java
@@ -1,6 +1,8 @@
 package com.mandeep.ims;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
 import com.mandeep.ims.entity.ItemType;
@@ -12,12 +14,14 @@ public class IntialSetup {
 	@Autowired
 	private ItemTypeRepository itemTypeRepository;
 
-	public IntialSetup() {
+	@EventListener
+    public void appReady(ApplicationReadyEvent event) {
+
 		if (itemTypeRepository.findByName("Mineral Water") == null)
 			itemTypeRepository.save(new ItemType("Mineral Water"));
 
 		if (itemTypeRepository.findByName("Reverse Osmosis") == null)
 			itemTypeRepository.save(new ItemType("Reverse Osmosis"));
-
-	}
+    }
+	
 }
